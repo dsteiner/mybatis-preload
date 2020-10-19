ca7c6e533e3c389e03112c4c6810a625a1afc571
dieter <dieter@steiner-olching.de>
2020-10-19 23:32:10 +0200

    Updates: Maven version upgrades and lknks in README

---
+++ README.md
@@ -0,0 +1,26 @@
+[myBatis](https://mybatis.org) - Implementation of the PRELOAD-Pattern
+-----------------------------------------------
+
+This example shows how the 'Preload-Pattern' could be implemented for myBatis_ 
+
+> myBatis: http://www.mybatis.org/
+
+# Features 
+
+
+- resolves 1:1 proxy entities ('ghost's) 
+- resolves 1:n relations
+- deep recursive resolving 
+
+#Using...
+
+
+- maven
+- Spring
+- 'hsqldb' for test data 
+
+See also about implementation with Hibernate:
+
+> Java-Magazin: java-magazin Ausgabe-4/2008
+
+> Benjamin_Winterberg: https://winterbe.com/posts/2009/09/02/hibernate-preload-pattern/
--- src/test/java/de/dst/mybatis/service/AccountServiceTest.java
+++ src/test/java/de/dst/mybatis/service/AccountServiceTest.java
@@ -3,13 +3,14 @@ package de.dst.mybatis.service;
 import java.util.List;
 
 import org.apache.commons.lang.Validate;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import de.dst.mybatis.domain.Account;
@@ -21,7 +22,7 @@ import de.dst.mybatis.domain.SignonGhost;
 import de.dst.mybatis.preload.Preload;
 import de.dst.mybatis.preload.PreloadFindMethod;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = { "/applicationContext.xml" })
 public class AccountServiceTest {
 	@Autowired
@@ -33,26 +34,26 @@ public class AccountServiceTest {
 	public void loadFieldType() {
 		FieldType ft = accountService
 				.getFieldType("8791fd99-efca-347c-eeb9-52988a95c453");
-		Assert.assertNotNull(ft);
-		Assert.assertFalse(ft instanceof Ghost);
+		Assertions.assertNotNull(ft);
+		Assertions.assertFalse(ft instanceof Ghost);
 	}
 
 	@Test
 	public void loadAccount() {
 		Account account = accountService.getAccount("j2ee");
-		Assert.assertNotNull(account);
+		Assertions.assertNotNull(account);
 		Signon signon = account.getSignon();
-		Assert.assertNotNull(signon);
-		Assert.assertTrue(signon instanceof SignonGhost);
-		Assert.assertNotNull(signon.getId());
+		Assertions.assertNotNull(signon);
+		Assertions.assertTrue(signon instanceof SignonGhost);
+		Assertions.assertNotNull(signon.getId());
 		logger.debug("loaded Account: " + account.toString());
 	}
 
 	@Test
 	public void loadSignonByUsername() {
 		Signon signon = accountService.getSignonByUsername("j2ee");
-		Assert.assertNotNull(signon);
-		Assert.assertFalse(signon instanceof Ghost);
+		Assertions.assertNotNull(signon);
+		Assertions.assertFalse(signon instanceof Ghost);
 		logger.debug("loaded Account: " + signon.toString());
 	}
 
@@ -73,7 +74,7 @@ public class AccountServiceTest {
 				preloadFindMethod);
 		Account account = accountService.getAccount("j2ee", preload);
 		Validate.notNull(account.getSignon());
-		Assert.assertFalse(account.getSignon() instanceof Ghost);
+		Assertions.assertFalse(account.getSignon() instanceof Ghost);
 	}
 
 	@Test
@@ -88,7 +89,7 @@ public class AccountServiceTest {
 		Validate.notEmpty(extraFields);
 
 		AccountField af1 = extraFields.get(0);
-		Assert.assertTrue(af1.getType() instanceof Ghost);
+		Assertions.assertTrue(af1.getType() instanceof Ghost);
 	}
 
 	@Test
@@ -122,6 +123,6 @@ public class AccountServiceTest {
 		List<AccountField> extraFields = account.getExtraFields();
 		Validate.notEmpty(extraFields);
 		AccountField af1 = extraFields.get(0);
-		Assert.assertFalse(af1.getType() instanceof Ghost);
+		Assertions.assertFalse(af1.getType() instanceof Ghost);
 	}
 }
